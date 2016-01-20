6e97ca30a2371e272eb3934401a0805dd7b71eaa
peter-s <peter_future@outlook.com>
2016-01-20 21:59:14 +0800

    [hls] release v0.1

--- test/hls_test.c
+++ test/hls_test.c
@@ -18,11 +18,17 @@
 
 int main(int argc, char **argv)
 {
+    if (argc < 2) {
+        printf("test url \n");
+        return 0;
+    }
+
     printf("===========================================\n");
     printf("Enter HLS Unit Test\n");
     //const char *uri = "http://devimages.apple.com/iphone/samples/bipbop/gear1/prog_index.m3u8";
     //const char *uri = "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8";
-    const char *uri = "http://10.18.19.52:8000/HLS_V5_TEST/index.m3u8";
+    //const char *uri = "http://10.18.19.52:8000/HLS_V5_TEST/index.m3u8";
+    const char *uri = argv[1];
     void *hls_ctx;
     dthls_open(&hls_ctx, uri);
     dt_av_pkt_t pkt;
